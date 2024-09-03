8c446b41e54a7744bf3317c3ea5fb8b02ebc2dc8
Mikhail <eremin.ms92@gmail.com>
2024-09-03 11:44:01 +0300

    Remove redundant help texts from help.go
    The hardcoded help texts for individual commands have been removed to streamline the codebase. This ensures maintainability by centralizing help content management.

--- help.go
+++ help.go
@@ -10,37 +10,3 @@ expense-tracker summary [--month <number>] [--year <number>]
 
 add --help to any command to get detailed information
 `
-
-const AddHelpText = `expense-tracker add --description <description> --amount <amount>
-
-add a new record to the tracker
---description <string>    text description, required
---amount <number>         money amount, required, must be more than 0
-`
-
-const UpdateHelpText = `expense-tracker update --id <id> [--description <description>] [--amount <amount>]
-
-set new description and/or amount to record with specified id, at least one optional parameter must be specified
---id <id>                 record id, required
---description <string>    text description, optional
---amount <number>         money amount, optional, must be more than 0
-`
-
-const DeleteHelpText = `expense-tracker delete --id <id>
-
-delete record with specified id
-`
-
-const ListHelpText = `expense-tracker list
-
-list all tracked records
-`
-
-const SummaryHelpText = `expense-tracker summary [--month [number]] [--year [number]]
-
-show total expenses for all time, can set optional parameters to show total expenses for specified period
---month             show total expenses for the current month
---month <number>    show total expenses for the specified month (from 1 to 12)
---year              show total expenses for the current year
---year  <number>    show total expenses for the specified year
-`
